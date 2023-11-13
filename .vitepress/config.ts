fee690b795b5d84440a27900ef5c4fb93537d015
katarn <katarn@katarn.es>
2023-11-13 17:29:15 +0100

    move md root to pages

---
+++ .vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Antonio Gonzalez Gea - Desarrollador Full Stack',
+  description: 'Portfolio de Antonio Gonzalez Gea, desarrollador full stack y aficionado a la impresión 3D. Mi' +
+    ' trayectoria profesional en desarrollo, mis creaciones en impresión 3D y otros hobbies que inspiran mi trabajo.',
+  srcDir: 'pages'
+})
