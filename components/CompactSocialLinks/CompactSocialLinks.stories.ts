1f95b29402cffecddf0cecc5ded87e624025a59b
katarn <katarn@katarn.es>
2023-12-02 11:48:56 +0100

    add types to stories

--- components/CompactSocialLinks/CompactSocialLinks.stories.ts
+++ components/CompactSocialLinks/CompactSocialLinks.stories.ts
@@ -1,12 +1,13 @@
 import CompactSocialLinks from './CompactSocialLinks.vue'
+import { type Meta, type StoryObj } from '@storybook/vue3'
 
 export default {
   component: CompactSocialLinks,
   tags: ['autodocs'],
   argTypes: {},
-}
+} satisfies Meta<typeof CompactSocialLinks>
 
-export const AllEnabled = {
+export const AllEnabled: StoryObj<typeof CompactSocialLinks> = {
   args: {
     linkedInHandle: 'antonio-gonzalez-gea',
     xHandle: 'Tito_Kati',
@@ -15,25 +16,25 @@ export const AllEnabled = {
   },
 }
 
-export const OnlyLinkedIn = {
+export const OnlyLinkedIn: StoryObj<typeof CompactSocialLinks> = {
   args: {
     linkedInHandle: 'antonio-gonzalez-gea',
   },
 }
 
-export const OnlyX = {
+export const OnlyX: StoryObj<typeof CompactSocialLinks> = {
   args: {
     xHandle: 'Tito_Kati',
   },
 }
 
-export const OnlyGitHub = {
+export const OnlyGitHub: StoryObj<typeof CompactSocialLinks> = {
   args: {
     gitHubHandle: 'Tito-Kati',
   },
 }
 
-export const OnlyPrintables = {
+export const OnlyPrintables: StoryObj<typeof CompactSocialLinks> = {
   args: {
     printablesHandle: 'Katarn',
   },
--- components/SocialLinks/SocialLinks.stories.ts
+++ components/SocialLinks/SocialLinks.stories.ts
@@ -1,12 +1,13 @@
 import SocialLinks from './SocialLinks.vue'
+import { type Meta, type StoryObj } from '@storybook/vue3'
 
 export default {
   component: SocialLinks,
   tags: ['autodocs'],
   argTypes: {},
-}
+} satisfies Meta<typeof SocialLinks>
 
-export const AllEnabled = {
+export const AllEnabled: StoryObj<typeof SocialLinks> = {
   args: {
     customLink: 'https://bashunit.typeddevs.com/',
     customImage: 'https://bashunit.typeddevs.com/logo.svg',
@@ -19,43 +20,43 @@ export const AllEnabled = {
   },
 }
 
-export const OnlyLinkedIn = {
+export const OnlyLinkedIn: StoryObj<typeof SocialLinks> = {
   args: {
     linkedInHandle: 'antonio-gonzalez-gea',
   },
 }
 
-export const OnlyX = {
+export const OnlyX: StoryObj<typeof SocialLinks> = {
   args: {
     xHandle: 'Tito_Kati',
   },
 }
 
-export const OnlyGitHubUser = {
+export const OnlyGitHubUser: StoryObj<typeof SocialLinks> = {
   args: {
     gitHubHandle: 'Tito-Kati',
   },
 }
 
-export const OnlyGitHubRepository = {
+export const OnlyGitHubRepository: StoryObj<typeof SocialLinks> = {
   args: {
     gitHubHandle: 'Tito-Kati/Huezzle',
   },
 }
 
-export const OnlyPrintablesUser = {
+export const OnlyPrintablesUser: StoryObj<typeof SocialLinks> = {
   args: {
     printablesHandle: 'Katarn',
   },
 }
 
-export const OnlyPrintablesModel = {
+export const OnlyPrintablesModel: StoryObj<typeof SocialLinks> = {
   args: {
     printablesModelId: 421668,
   },
 }
 
-export const OnlyCustomBashunit = {
+export const OnlyCustomBashunit: StoryObj<typeof SocialLinks> = {
   args: {
     customLink: 'https://bashunit.typeddevs.com/',
     customImage: 'https://bashunit.typeddevs.com/logo.svg',
@@ -63,7 +64,7 @@ export const OnlyCustomBashunit = {
   },
 }
 
-export const OnlyCustomHuezzle = {
+export const OnlyCustomHuezzle: StoryObj<typeof SocialLinks> = {
   args: {
     customLink: 'https://huezzle.antonio.gg/',
     customImage: 'https://huezzle.antonio.gg/img/icons/favicon.svg',
